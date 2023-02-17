4203cb70d317df1943b7bebb8050ebfa270729be
Krisnaputra15 <41879957+Krisnaputra15@users.noreply.github.com>
2023-02-17 14:34:31 +0700

    created route file for user

--- main.go
+++ main.go
@@ -1,1 +1,21 @@
 package main
+
+import (
+	"github.com/Krisnaputra15/go-tweet-echo/config"
+	"github.com/Krisnaputra15/go-tweet-echo/db"
+	"github.com/Krisnaputra15/go-tweet-echo/route"
+	"github.com/labstack/echo/v4"
+)
+
+func init() {
+	config.LoadEnv()
+	db.ConnectToDB()
+}
+
+func main() {
+	e := echo.New()
+
+	route.InitRoute()
+
+	e.Logger.Fatal(e.Start(":3000"))
+}
